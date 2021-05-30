c2ca97360b380a8ae41288b4f0bcc26188b25540
Jugesh Raghav <jugesh1520raghav@gmail.com>
2021-05-30 13:09:45 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+
+const button = document.querySelector("#add-button");
+
+const addNewNote = (text = "jugesh") => {
+    const container = document.createElement('div');
+    container.classList.add("container");
+    const htmlData = `
+    <div class="icons">
+            <button id="edit-button"> <i class="fas fa-edit"></i></button>
+            <button id="delete-button"><i class="fas fa-trash-alt"></i></button>    
+    </div>
+
+        <div class="main ${text ? "" : "hidden"}"></div>
+        <textarea class= "${text ? "" : "hidden"}"></textarea>
+    
+    `
+  
+    container.insertAdjacentHTML("afterbegin", htmlData);
+    
+    //getting the references
+    const editButton = container.querySelector("#edit-button");
+    const deleteButton = container.querySelector("#delete-button");
+    const main = container.querySelector(".main");
+    const textarea = container.querySelector("textarea");
+
+    //delete the note
+    deleteButton.addEventListener("click", () => {
+        container.remove();
+    });
+
+    //edit the note
+    editButton.addEventListener("click", () => {
+        main.classList.toggle("hidden");
+        textarea.classList.toggle("hidden");
+    });
+
+   
+    document.body.appendChild(container);
+}
+
+//add new note
+button.addEventListener("click", addNewNote);
+
